da1805a9b075b2f53e09075b018f56e1a0f55eb4
harshubari <121845828+harshubari@users.noreply.github.com>
2024-02-26 11:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 //your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-
-function removehmm(str){
-	let arr=[ "the","The","a","A","an","An"];
-	return newstr.trim();
-}
-bands.sort((a,b) => removehmm(a) > removehmm(b) ? 1 : -1);
-console.log(bands);
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skyliconst bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+        function sortArticles(articles) {
+            return articles.sort((a, b) => {
+                const ignoreWords = /^(a|an|the)\s/i;
+                const cleanA = a.replace(ignoreWords, '');
+                const cleanB = b.replace(ignoreWords, '');
+                return cleanA.localeCompare(cleanB);
+            });
+        }
+        const sortedBands = sortArticles(bands);
+        const ul = document.getElementById('band');
+        sortedBands.forEach(article => {
+            const li = document.createElement('li');
+            li.textContent = article;
+            ul.appendChild(li);
+        });
