546385d1d1b88308c3c724fa1861a091bd9b83ce
harshubari <121845828+harshubari@users.noreply.github.com>
2024-02-26 12:11:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-let ul= document.querySelector("#band");
+let ul= document.querySelector("#bands");
    function sortArticles(articles) {
             return articles.sort((a, b) => {
                 const ignoreWords = /^(a|an|the)\s/i;
