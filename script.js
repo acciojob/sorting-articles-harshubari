fc76fd4a60706426ab5c835aa74a8d4e81783d2b
harshubari <121845828+harshubari@users.noreply.github.com>
2024-02-26 11:36:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function removehmm(str){
+	let arr=[ "the","The","a","A","an","An"];
+	return newstr.trim();
+}
+bands.sort((a,b) => removehmm(a) > removehmm(b) ? 1 : -1);
+console.log(bands);
